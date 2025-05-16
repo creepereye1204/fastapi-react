12d2188da1e16f3b064f71003bd825825c2ae36f
Creeper Eye <iostream1204@gmail.com>
2025-05-16 14:32:13 +0000

    added video

--- frontend/src/pages/Video.tsx
+++ frontend/src/pages/Video.tsx
@@ -7,84 +7,27 @@ import React, { useEffect } from "react";
 
 const Video = () => {
   useEffect(() => {
-    const demosSection = document.getElementById("demos");
-
     let poseLandmarker: any = undefined;
     let runningMode: any = "VIDEO";
     let enableWebcamButton: any;
     let webcamRunning: Boolean = false;
     const videoHeight: string = "360px";
     const videoWidth: string = "480px";
-
+    const wasmAssetPath =
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22-rc.20250304/wasm";
+    const modelAssetPath = `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/latest/pose_landmarker_lite.task`;
     const createPoseLandmarker = async () => {
-      const vision = await FilesetResolver.forVisionTasks(
-        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22-rc.20250304/wasm"
-      );
+      const vision = await FilesetResolver.forVisionTasks(wasmAssetPath);
       poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
         baseOptions: {
-          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/latest/pose_landmarker_lite.task`,
+          modelAssetPath: modelAssetPath,
           delegate: "GPU",
         },
         runningMode: runningMode,
         numPoses: 2,
       });
-      demosSection?.classList.remove("invisible");
     };
     createPoseLandmarker();
-
-    const imageContainers = document.getElementsByClassName("detectOnClick");
-
-    for (let i = 0; i < imageContainers.length; i++) {
-      imageContainers[i].children[0].addEventListener("click", handleClick);
-    }
-
-    async function handleClick(event: any) {
-      if (!poseLandmarker) {
-        console.log("Wait for poseLandmarker to load before clicking!");
-        return;
-      }
-
-      //   if (runningMode === "VIDEO") {
-      //     runningMode = "IMAGE";
-      //     await poseLandmarker.setOptions({ runningMode: "IMAGE" });
-      //   }
-
-      const allCanvas =
-        event.target.parentNode.getElementsByClassName("canvas");
-      for (var i = allCanvas.length - 1; i >= 0; i--) {
-        const n = allCanvas[i];
-        n.parentNode.removeChild(n);
-      }
-
-      poseLandmarker.detect(event.target, (result: any) => {
-        const canvas = document.createElement("canvas");
-        canvas.setAttribute("class", "canvas");
-        canvas.setAttribute("width", event.target.naturalWidth + "px");
-        canvas.setAttribute("height", event.target.naturalHeight + "px");
-        canvas.style =
-          "left: 0px;" +
-          "top: 0px;" +
-          "width: " +
-          event.target.width +
-          "px;" +
-          "height: " +
-          event.target.height +
-          "px;";
-
-        event.target.parentNode.appendChild(canvas);
-        const canvasCtx: any = canvas.getContext("2d");
-        const drawingUtils = new DrawingUtils(canvasCtx);
-        for (const landmark of result.landmarks) {
-          drawingUtils.drawLandmarks(landmark, {
-            radius: (data) => DrawingUtils.lerp(data.from!.z, -0.15, 0.1, 5, 1),
-          });
-          drawingUtils.drawConnectors(
-            landmark,
-            PoseLandmarker.POSE_CONNECTIONS
-          );
-        }
-      });
-    }
 
     const video = document.getElementById("webcam") as HTMLVideoElement;
     const canvasElement = document.getElementById(
@@ -134,10 +77,6 @@ const Video = () => {
         canvasElement.style.width = videoWidth;
         video.style.width = videoWidth;
 
-        // if (runningMode === "IMAGE") {
-        //   runningMode = "VIDEO";
-        //   await poseLandmarker.setOptions({ runningMode: "VIDEO" });
-        // }
         let startTimeMs = performance.now();
         if (lastVideoTime !== video.currentTime) {
           lastVideoTime = video.currentTime;
@@ -173,41 +112,27 @@ const Video = () => {
   }, []);
   return (
     <div className="video">
-      <h1>Pose detection using the MediaPipe PoseLandmarker task</h1>
+      <div id="liveView" className="videoView">
+        <button id="webcamButton" className="mdc-button mdc-button--raised">
+          <span className="mdc-button__label">ENABLE WEBCAM</span>
+        </button>
+        <div style={{ position: "relative" }}>
+          <video
+            id="webcam"
+            style={{ width: "1280px", height: "720px", position: "absolute" }}
+            autoPlay
+            playsInline
+          ></video>
 
-      <section id="demos" className="invisible">
-        <h2>Demo: Detecting Images</h2>
-        <p>
-          <b>Click on an image below</b> to see the key landmarks of the body.
-        </p>
-
-        {/* liveView div에 className 적용 */}
-        <div id="liveView" className="videoView">
-          {/* 버튼에 className 적용 */}
-          <button id="webcamButton" className="mdc-button mdc-button--raised">
-            {/* span 태그에 className 적용 */}
-            <span className="mdc-button__ripple"></span>
-            <span className="mdc-button__label">ENABLE WEBCAM</span>
-          </button>
-          <div style={{ position: "relative" }}>
-            {/* video 태그 style 속성을 객체로 변경, autoPlay, playsInline camelCase 적용, position 오타 수정 */}
-            <video
-              id="webcam"
-              style={{ width: "1280px", height: "720px", position: "absolute" }}
-              autoPlay
-              playsInline
-            ></video>
-            {/* canvas 태그 style 속성을 객체로 변경 */}
-            <canvas
-              className="output_canvas"
-              id="output_canvas"
-              width="1280"
-              height="720"
-              style={{ position: "absolute", left: "0px", top: "0px" }}
-            ></canvas>
-          </div>
+          <canvas
+            className="output_canvas"
+            id="output_canvas"
+            width="1280"
+            height="720"
+            style={{ position: "absolute", left: "0px", top: "0px" }}
+          ></canvas>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
